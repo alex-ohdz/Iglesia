5547f34c79dcb3f6896482a9b1110168647111a9
Alejandro Oramas H <alejandrooramash@gmail.com>
2024-07-15 16:21:20 -0400

    secret4

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,8 +1,16 @@
+import BtnHome from "@components/btnHome";
+
 export default function SuccessPage() {
   return (
+    <>
     <div className="p-4 bg-gray-100 rounded-md shadow-md">
       <h1 className="text-3xl font-bold mb-4">¡Gracias por tu donación!</h1>
       <p>Tu donación ha sido procesada exitosamente.</p>
+
     </div>
+    <div>
+      <BtnHome/>
+    </div>
+    </>
   );
 }
