ef9cca8c5eb4bd98ce6356b02108248ec8331b76
alex-ohdz <alejandrooramash@gmail.com>
2024-10-27 18:38:38 +0100

    Descripción de los cambios realizados

--- pages/api/getCarousel.js
+++ pages/api/getCarousel.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { query } from "@/lib/db";
 
 export default async function handler(req, res) {
@@ -13,3 +14,20 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Error fetching images" });
   }
 }
+=======
+// import { query } from "@/lib/db";
+
+// export default async function handler(req, res) {
+//   if (req.method !== "GET") {
+//     return res.status(405).json({ error: `Method ${req.method} not allowed` });
+//   }
+
+//   try {
+//     const result = await query("SELECT * FROM home_carousel");
+//     res.status(200).json(result.rows);
+//   } catch (error) {
+//     console.error("Error fetching images", error);
+//     res.status(500).json({ error: "Error fetching images" });
+//   }
+// }
+>>>>>>> 7f8deaf (Descripción de los cambios realizados)
