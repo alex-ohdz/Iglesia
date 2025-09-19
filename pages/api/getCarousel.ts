de746229e231265ce56cecd52533ed0b82b7dc9d
Alejandro Oramas Hdz <99675215+alex-ohdz@users.noreply.github.com>
2025-09-20 00:26:07 +0200

    Fix carousel API merge conflict

--- pages/api/getCarousel.ts
+++ pages/api/getCarousel.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { query } from "@/lib/db";
 
 export default async function handler(req, res) {
@@ -7,42 +5,11 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: `Method ${req.method} not allowed` });
   }
 
-  try {
-    const result = await query("SELECT * FROM home_carousel");
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error("Error fetching images", error);
-    res.status(500).json({ error: "Error fetching images" });
-  }
-}
-=======
-// import { query } from "@/lib/db";
-=======
-import { query } from "@/lib/db";
->>>>>>> fca4660 (cambiando a sql)
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: `Method ${req.method} not allowed` });
-  }
-
-<<<<<<< HEAD
-//   try {
-//     const result = await query("SELECT * FROM home_carousel");
-//     res.status(200).json(result.rows);
-//   } catch (error) {
-//     console.error("Error fetching images", error);
-//     res.status(500).json({ error: "Error fetching images" });
-//   }
-// }
->>>>>>> 7f8deaf (Descripción de los cambios realizados)
-=======
   try {
     const result = await query("SELECT * FROM carrousel");
-    res.status(200).json(result.rows);
+    res.status(200).json(result);
   } catch (error) {
     console.error("Error fetching images", error);
     res.status(500).json({ error: "Error fetching images" });
   }
 }
->>>>>>> fca4660 (cambiando a sql)
